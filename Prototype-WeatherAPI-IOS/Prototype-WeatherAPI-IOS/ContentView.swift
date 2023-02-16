56c043aae1bd0ef77eddc7cd8f12d0414c196a5f
Reno Muijsenberg <r.muijsenberg@student.fontys.nl>
2023-02-16 13:16:36 +0100

    Add weather of Maasbree in C

--- Prototype-WeatherAPI-IOS/Prototype-WeatherAPI-IOS/ContentView.swift
+++ Prototype-WeatherAPI-IOS/Prototype-WeatherAPI-IOS/ContentView.swift
@@ -1,21 +1,70 @@
-//
-//  ContentView.swift
-//  Prototype-WeatherAPI-IOS
-//
-//  Created by Reno Muijsenberg on 16/02/2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    let apiKey = "10d75ce922ba55bf2abd1dae42d58e54"
+    let city = "Maasbree"
+    let units = "metric"
+    
+    @State var temperature: Double?
+    @State var description: String?
+    @State var error: String?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if let temperature = temperature, let description = description {
+                Text("\(city): \(temperature)°C - \(description)")
+            } else if let error = error {
+                Text("Error: \(error)")
+            } else {
+                Text("Loading...")
+            }
         }
-        .padding()
+        .onAppear(perform: fetchData)
+    }
+    
+    func fetchData() {
+        if let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&units=\(units)&appid=\(apiKey)") {
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                // Check for errors
+                if let error = error {
+                    self.error = error.localizedDescription
+                    return
+                }
+                
+                // Check for a successful response
+                guard let httpResponse = response as? HTTPURLResponse,
+                      (200...299).contains(httpResponse.statusCode) else {
+                    self.error = "Invalid response"
+                    return
+                }
+                
+                // Check that we received data
+                guard let data = data else {
+                    self.error = "No data received"
+                    return
+                }
+                
+                // Parse the JSON response
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                        // Update the UI with the data
+                        if let main = json["main"] as? [String: Any],
+                           let temp = main["temp"] as? Double {
+                            self.temperature = temp
+                        }
+                        
+                        if let weatherArray = json["weather"] as? [[String: Any]],
+                           let weather = weatherArray.first,
+                           let description = weather["description"] as? String {
+                            self.description = description
+                        }
+                    }
+                } catch {
+                    self.error = error.localizedDescription
+                }
+            }
+            task.resume()
+        }
     }
 }
 
