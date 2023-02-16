510c2f2aee59028c73163addf74785299f542b21
Reno Muijsenberg <r.muijsenberg@student.fontys.nl>
2023-02-16 12:00:24 +0100

    First commit

---
+++ Prototype-WeatherAPI-IOS/Prototype-WeatherAPI-IOS/Prototype_WeatherAPI_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prototype_WeatherAPI_IOSApp.swift
+//  Prototype-WeatherAPI-IOS
+//
+//  Created by Reno Muijsenberg on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Prototype_WeatherAPI_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
